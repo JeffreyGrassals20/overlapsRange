fb510c4acd55ab574e69d504ce59d17c8a64dee2
Jeffrey <slassarg20@gmail.com>
2021-06-22 22:15:38 -0400

    Project tested and missing some validations

--- overlapsRange/RangeC.cs
+++ overlapsRange/RangeC.cs
@@ -5,7 +5,51 @@ namespace overlapsRange
 
     public class RangeC
     {
-      
+        private readonly int _realMin;
+        private readonly int _realMax;
+        private char[] remove = { '[', ']', '(', ')', ',' };
+
+        public RangeC (string range)
+        {
+            (_realMin, _realMax) = LaMagiaDeJeffrey(range);
+        }
+
+        private (int _realMin, int _realMax) LaMagiaDeJeffrey(string range)
+        {
+            string [] arrRange = range.Split(remove, StringSplitOptions.RemoveEmptyEntries);
+
+            int value = 0;
+            int valuemax = 0;
+            if(range[0] == '(')
+            {
+                value = int.Parse(arrRange[0]) + 1;
+                arrRange[0] = value.ToString();
+
+            }
+
+            if (range[^1] == ')')
+            {
+                valuemax = int.Parse(arrRange[^1]) - 1;
+                arrRange[^1] = valuemax.ToString();
+            }
+
+            return (int.Parse(arrRange[0]), int.Parse(arrRange[^1]));
+        }
+
+        public bool overlapsWith(RangeC range)
+        {
+
+            if (_realMax == range._realMax)
+                return true;
+            
+            if (_realMin == range._realMin)
+                return true;
+
+            if (range._realMax > _realMin)
+                return true;
+
+            return false;
+        }
 
     }
 }
--- tests/UnitTest1.cs
+++ tests/UnitTest1.cs
@@ -7,8 +7,11 @@ namespace tests
     {
 
         [Test]
-        public void test1()
+        public void Range2to5_Overlaps_With_Range3to6()
         {
+            RangeC r = new RangeC("[2,5)");
+            RangeC x = new RangeC("[3,6)");
+            Assert.IsTrue(r.overlapsWith(x));
             
         }
  
